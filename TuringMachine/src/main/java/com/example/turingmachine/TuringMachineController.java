cccda5bf5f35ac331f5205c7956844f12602a3fb
Oliatkach <otkac310@gmail.com>
2023-12-03 01:47:14 +0200

    [commit 2] Interface

--- TuringMachine/src/main/java/com/example/turingmachine/TuringMachineController.java
+++ TuringMachine/src/main/java/com/example/turingmachine/TuringMachineController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TuringMachineController {
-    @GetMapping("machine")
+    @GetMapping("emulator")
     String machine(){
-        return "machine";
+        return "emulator";
     }
 
     @GetMapping("docs")
